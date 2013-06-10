b1b4a73099da953be540f79b75ba6037c92a7a4b
Kiril Vladimirov <kiril@vladimiroff.org>
2013-06-10 11:47:59 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='labyrinpy',
+    version='0.1',
+    description='Implementation of Labyrintti SMS Gateway API',
+    author='Kiril Vladimiroff',
+    author_email='kiril@vladimiroff.org',
+    url='https://github.com/Vladimiroff/labyrinpy/',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=['requests>=1.2.3'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
+)
